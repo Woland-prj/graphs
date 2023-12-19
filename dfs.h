ee197a5e68a6430f28faf68ba752e7ec553d7d9c
Woland-prj <nzimin93@gmail.com>
2023-12-19 09:34:34 +0300

    возвпащение dfs

---
+++ dfs.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "convert.h"
+#include <stack>
+
+using TVisited = std::vector<int>;
+
+bool CheckVisited(TVisited& visited, int top)
+{
+    for(int i : visited)
+        if(i == top)
+            return true;
+    return false;
+}
+
+void DFS(CMatrix& dataMatrix, int top = 0)
+{
+    std::stack<int> st;
+    TVisited visited;
+    st.push(top);
+    visited.clear();
+    while(!st.empty())
+    {
+        top = st.top();
+        st.pop();
+        for(int i = 0; i < dataMatrix.matrixData.size(); i++)
+            if(dataMatrix.matrixData[top][i] > 0 && !CheckVisited(visited, i))
+            {
+                st.push(i);
+                visited.push_back(top);
+                std::cout << "Вершина: " << top + 1 << " Связаннные с ней вершины: "<< i + 1 << std::endl;
+            }
+    }
+}
